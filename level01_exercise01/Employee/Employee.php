f896b4c70673c64a38f13d12cc6c7711238fcd5e
Mar <marpouradressa@gmail.com>
2026-02-19 13:00:05 +0100

    Fixing the bug frfr

--- level01_exercise01/Employee/Employee.php
+++ level01_exercise01/Employee/Employee.php
@@ -1,27 +1,26 @@
 <?php
 
-class Employee
-{
+class Employee {
 
-    private string $name;
-    private int $wage;
+private string $name; 
+private int $wage; 
 
 
-    public function setEmployee(string $name, int $wage)
-    {
-        $this->name = $name;
-        $this->wage = $wage;
-    }
-
-    public function getDetails(): string
-    {
-        if ($this->wage >= 6000) {
-            return "Employee's name is " . $this->name
-                . " and their wage is " . $this->wage . " so they need to pay taxes.\n";
-        } else {
-            return "Employee's name is " . $this->name
-                . " and their wage is " . $this->wage . " so they don't need to pay taxes.\n";
-        }
-    }
+public function __construct($name, $wage) {
+    $this->name = $name;
+    $this->wage = $wage;
 }
+
+public function getDetails(): string {
+    if ($this->wage > 6000) {
+        return "Employee's name is " . $this->name
+         . " and their wage is " . $this->wage . " so they need to pay taxes.\n";
+    } else {
+        return "Employee's name is " . $this->name
+         . " and their wage is " . $this->wage . " so they don't need to pay taxes.\n";
+    } 
+}
+
+}
+
 ?>
--- level01_exercise01/exercise01.php
+++ level01_exercise01/exercise01.php
@@ -2,16 +2,13 @@
 
 require_once "Employee/Employee.php"; 
 
-$Emp1 = new Employee();
-$Emp1->setEmployee("Mark", 8000);
+$Emp1 = new Employee("Mark", 8000);
 echo $Emp1->getDetails(); 
 
-$Emp2 = new Employee();
-$Emp2->setEmployee("Sara", 6200);
+$Emp2 = new Employee("Sara", 6200);
 echo $Emp2->getDetails();
 
-$Emp3 = new Employee();
-$Emp3->setEmployee("Kane", 2500);
+$Emp3 = new Employee("Kane", 2500);
 echo $Emp3->getDetails();
 
 ?>
